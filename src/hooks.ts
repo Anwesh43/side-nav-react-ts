4d0999b9fbdc6ca89d88e032df0886fccd827bc4
Anwesh43 <anweshthecool0@gmail.com>
2021-05-08 18:03:24 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,11 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20
 export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
+    const [dir, setDir] = useState(1)
     return {
         scale, 
         start() {
@@ -15,7 +16,8 @@ export const useAnimatedScale = () => {
                         if (scale > 1) {
                             setAnimated(false)
                             clearInterval(interval)
-                            return 0
+                            setDir((prevDir : number) => prevDir * -1)
+                            return 1
                         }
                         return prev + scGap 
                     })
@@ -37,5 +39,53 @@ export const useDimension = () => {
     return {
         w, 
         h
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const maxX : number = w < 640 ? w * 0.6 : w * 0.33
+    const x : number = maxX * scale 
+    const size : number = Math.min(w, h) / 15 
+    return {
+        parentStyle() {
+            const left : string = `${x}px`
+            return {
+                position,
+                left 
+            }
+        },
+        parentHamburgStyle() {
+            return {
+                position, 
+                top : `${size / 2}px`
+
+            }
+        },
+        lineStyle(i : number, j : number) : CSSProperties {
+            const top = `${(-size * 0.5 + size * 0.5 * i ) * (1 - scale)}px`
+            const transform = `rotate(${45 * scale * (1 - 2 * j)})deg`
+            const left = `${size / 10}px`
+            return {
+                top,
+                left,
+                transform
+            }
+        },
+        sideNavStyle() {
+            const zIndex : number = 1000
+            const boxShadow = `10px 10px rgba(0, 0, 0, 0.5)`
+            const left : string = `${-x + x * scale}px`
+            const width = `${size}px`
+            const height = `${h}px`
+            const overflow = 'scroll'
+            return {
+                zIndex, 
+                boxShadow, 
+                left, 
+                width,
+                overflow
+            }
+        }
     }
 }
